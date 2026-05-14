bbedc0fdfcf2cbd938f6e04090afdb16744fdeb5
Takayuki Yoshimura <t.yoshimura@astroscale.com>
2026-05-15 04:56:46 +0900

    [update]keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "H", "H", { noremap = true, silent = true, desc = "Cursor to top of screen" })
+vim.keymap.set("n", "L", "L", { noremap = true, silent = true, desc = "Cursor to bottom of screen" })
+
+vim.keymap.set("n", "<C-h>", "<cmd>bprevious<cr>", { noremap = true, silent = true, desc = "Previous buffer" })
+vim.keymap.set("n", "<C-l>", "<cmd>bnext<cr>", { noremap = true, silent = true, desc = "Next buffer" })
